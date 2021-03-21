abd00213b60271d5c976cf6f65e960f5a05bd26f
Anton Kravchenko <anton.kravchenko@dish.com>
2021-03-21 21:20:13 +0200

    cleanup

--- deployment/shutdown-local.sh
+++ deployment/shutdown-local.sh
@@ -1,6 +1,5 @@
 helm delete my-mongo
 helm delete my-redis
-<<<<<<< HEAD
 helm delete my-kafka -n kafka
 
 kubectl delete -f 01-chat-service.yaml
@@ -15,8 +14,3 @@ kubectl delete --all pods -n kafka
 kubectl delete --all jobs -n kafka
 kubectl delete --all pvc -n kafka
 kubectl delete ns kafka
-=======
-kubectl delete -f 01-chat-service.yaml
-kubectl delete -f 02-client-app.yaml
-kubectl delete -f 03-user-service.yaml
->>>>>>> b8f221a98a5b1697dc777446629d1481b1c197ee
