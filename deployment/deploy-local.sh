1a810ba88d6ca2cbee6160bd529ed5ea4b182e7d
Anton Kravchenko <anton.kravchenko@dish.com>
2021-03-21 21:19:27 +0200

    cleanuop

--- deployment/deploy-local.sh
+++ deployment/deploy-local.sh
@@ -1,18 +1,12 @@
 helm install my-mongo bitnami/mongodb
 helm install my-redis --set cluster.slaveCount=0 bitnami/redis
-<<<<<<< HEAD
 kubectl create namespace kafka --dry-run=client -o yaml | kubectl apply -f -
 helm install my-kafka --namespace kafka -f 07-kafka-config.yaml bitnami/kafka
-=======
->>>>>>> b8f221a98a5b1697dc777446629d1481b1c197ee
 
 kubectl apply -f 01-chat-service.yaml
 kubectl apply -f 02-client-app.yaml
 kubectl apply -f 03-user-service.yaml
-<<<<<<< HEAD
 
 kubectl apply -f 04-traefik-rbac.yaml
 kubectl apply -f 05-traefik-deployment.yaml
 kubectl apply -f 06-ingress-routes.yaml
-=======
->>>>>>> b8f221a98a5b1697dc777446629d1481b1c197ee
