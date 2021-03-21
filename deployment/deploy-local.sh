c876fd01dc428a184c9e87ab38b87bb20317eac8
anton-kravchenko <antosha.kravchenko@gmail.com>
2021-03-21 21:20:47 +0200

    Added traefik

--- deployment/deploy-local.sh
+++ deployment/deploy-local.sh
@@ -1,6 +1,12 @@
 helm install my-mongo bitnami/mongodb
 helm install my-redis --set cluster.slaveCount=0 bitnami/redis
+kubectl create namespace kafka --dry-run=client -o yaml | kubectl apply -f -
+helm install my-kafka --namespace kafka -f 07-kafka-config.yaml bitnami/kafka
 
 kubectl apply -f 01-chat-service.yaml
 kubectl apply -f 02-client-app.yaml
 kubectl apply -f 03-user-service.yaml
+
+kubectl apply -f 04-traefik-rbac.yaml
+kubectl apply -f 05-traefik-deployment.yaml
+kubectl apply -f 06-ingress-routes.yaml
--- deployment/shutdown-local.sh
+++ deployment/shutdown-local.sh
@@ -1,5 +1,16 @@
 helm delete my-mongo
 helm delete my-redis
+helm delete my-kafka -n kafka
+
 kubectl delete -f 01-chat-service.yaml
 kubectl delete -f 02-client-app.yaml
 kubectl delete -f 03-user-service.yaml
+
+kubectl delete -f 04-traefik-rbac.yaml
+kubectl delete -f 05-traefik-deployment.yaml
+kubectl delete -f 06-ingress-routes.yaml
+
+kubectl delete --all pods -n kafka
+kubectl delete --all jobs -n kafka
+kubectl delete --all pvc -n kafka
+kubectl delete ns kafka
